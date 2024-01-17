f35736c6c158f48375ddd6202ca7644857223ae6
LucasVega2004 <luquitasvegablanco@gmail.com>
2024-01-17 19:26:34 +0000

    buscador implementado

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
 import { useState, useEffect } from 'react';
 import { useFetch } from '../hooks/useFetch';
 import { Link } from 'react-router-dom';
 import { useSearchParams } from 'react-router-dom';
+
 const Blog = () => {
   const { data, loading, error } = useFetch('https://rickandmortyapi.com/api/character');
-
+  const [searchTerm, setSearchTerm] = useState('');
   let [searchParams, setSearchParams] = useSearchParams();
 
   if (loading) return (<h1>Cargando...</h1>);
@@ -13,37 +14,41 @@ const Blog = () => {
 
   console.log(data.results + "data.results");
 
-  const handleChange = (e) =>{
-    setSearchParams({filter: e.target.value});
-  }
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+    setSearchParams({ filter: e.target.value });
+  };
+
   return (
     <>
       <h1>Blog - Elige un personaje</h1>
 
       <input
-      type='text'
-      name=''
-      onChange={handleChange}
-      className='form-control my-3'
-      alt='Buscador'
-      value={searchParams.get("filter" || "")}
+        type='text'
+        name=''
+        onChange={handleChange}
+        className='form-control my-3'
+        alt='Buscador'
+        value={searchTerm}
       ></input>
 
       <ul className='list-group'>
-        {
-          data.results.map(item => {
-            return (
-              <Link className="list-group-item"
-                key={item.id}
-                to={`/blog/${item.id}`}>{item.name}</Link>
-            )
-          })
-        }
-
+        {data.results
+          .filter((item) =>
+            item.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .map((item) => (
+            <Link
+              className='list-group-item'
+              key={item.id}
+              to={`/blog/${item.id}`}
+            >
+              {item.name}
+            </Link>
+          ))}
       </ul>
     </>
-
-  )
+  );
 }
 
-export default Blog
+export default Blog;
